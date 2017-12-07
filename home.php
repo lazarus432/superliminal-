5a3940916f7335c9e1229c476ef5ae24bea8fd39
Duex <deux02030@gmail.com>
2017-12-06 20:32:30 -0500

    style: update site heading

--- home.php
+++ home.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
     <header class="masthead" style="background-image: url('<?php echo get_template_directory_uri(); ?> /img/home-bg.jpg')">
-      <div class="overlay"></div>
+    <div class="overlay"></div>
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="site-heading">
-              <h1>Clean Blog</h1>
+              <h1>Nic Bold</h1>
               <span class="subheading">A Blog Theme by Start Bootstrap</span>
             </div>
           </div>
