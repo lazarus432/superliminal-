1aa43c438def666ecf6c2b7a2eba46cd6220e7e9
Duex <deux02030@gmail.com>
2017-12-06 20:33:09 -0500

    docs: updated nav bar

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Clean Blog</title>
+    <title>NB</title>
 
     <!-- Get scripts from functions.php -->
     <?php wp_head(); ?> 
@@ -15,14 +15,17 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   </head>
-  <body <?php body_class(); ?>>
+  <body id="page-top" <?php body_class(); ?>>
+
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">NB</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
           <?php
           $defaults = array(
             'container' => 'ul',
@@ -31,6 +34,7 @@
           );
           wp_nav_menu( $defaults );
           ?>
-        </ul>
+          </ul>
+        </div>
       </div>
     </nav>
