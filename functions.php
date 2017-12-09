eaa5adcd1f8774119e3ecd572edc3c5261ca39da
Duex <deux02030@gmail.com>
2017-12-09 17:58:13 -0500

    feat: added flip and type js libraries
    Added these two libraries to portfolio.

--- functions.php
+++ functions.php
@@ -23,6 +23,8 @@ function superliminal_theme_scripts() {
     wp_enqueue_style( 'font_awesome_css', get_template_directory_uri() . '/vendor/font-awesome/css/font-awesome.min.css');
     wp_enqueue_style( 'style_css', get_template_directory_uri() . '/style.css');
 
+    wp_enqueue_script( 'flip.min.js', get_template_directory_uri() . '/js/flip.min.js', array('jquery'), '', true);
+    wp_enqueue_script( 'typeit.min.js', get_template_directory_uri() . '/js/typeit.min.js', array('jquery'), '', true);
     wp_enqueue_script( 'extra.js', get_template_directory_uri() . '/js/script.js', array('jquery'), '', true);
     wp_enqueue_script( 'tether', get_template_directory_uri() . '/js/tether.min.js', array('jquery'), '', true);
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true);
